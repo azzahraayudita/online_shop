6c0c0d2a8a495f81fa66f05cfc657dbe77d2bdf0
Diva Nadya Pasha Ramadhani <141887234+Divauu@users.noreply.github.com>
2024-05-10 19:36:10 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn=pg_connect("host=localhost port=5432 dbname=test_database user=postgres password=112407");
-/* check connection */
-if (!$conn) {
-    echo "<h1>Doesn’t work</h1>";
-}
-?>
